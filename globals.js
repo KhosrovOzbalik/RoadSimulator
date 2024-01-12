a533f956613007cc9833d8cbab6ce9268aee1d46
Hüsrev <Mhozbalik@gmail.com>
2024-01-12 22:11:45 +0300

    Update globals.js

--- globals.js
+++ globals.js
@@ -1,8 +1,10 @@
 const GRID_SIZE = 100;
 
-const SELECTION_BUILDING_1 = "Building 1";
-const SELECTION_BUILDING_2 = "Building 2";
-const SELECTION_ROCK = "Rock";
-const SELECTION_TREE = "Tree";
+const selection = Object.freeze({
+    BUILDING_1: "Building 1",
+    BUILDING_2: "Building 2",
+    ROCK: "Rock",
+    TREE: "Tree",
+})
 
 /*Meshler zart zurt globaller buraya*/
