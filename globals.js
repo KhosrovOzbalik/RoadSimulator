9846ea4a9d7a03cdc19bfe2f0c666ac69ae03019
Hüsrev <Mhozbalik@gmail.com>
2024-01-12 21:51:26 +0300

    Create globals.js

---
+++ globals.js
@@ -0,0 +1,8 @@
+const GRID_SIZE = 100;
+
+const SELECTION_BUILDING_1 = "Building 1";
+const SELECTION_BUILDING_2 = "Building 2";
+const SELECTION_ROCK = "Rock";
+const SELECTION_TREE = "Tree";
+
+/*Meshler zart zurt globaller buraya*/
