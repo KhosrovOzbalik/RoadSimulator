09892ff57d2ebab801c88a404b8c31e47746421c
Hüsrev <Mhozbalik@gmail.com>
2024-01-16 06:28:57 +0300

    Descript Oku
    Fare track'in canvasa göre tekrar ayarlanması gerekiyor.
    Rock ve Tree için Mesh eklemek lazım.
    generate fonksiyonu içinde road construct kısmının doldurulması lazım

--- app.js
+++ app.js
@@ -1,49 +1,59 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+import {FBXLoader} from "three/examples/jsm/loaders/FBXLoader";
 
 import {GRID_SIZE, selection} from "./globals";
-import {AssetsObject, Building} from "./objects";
-import {addCity,graph,grid, removeCity, constructGraph} from "./datas";
+import {resetRoads, addRoad, AssetsObject, Building} from "./objects";
+import {addCity, graph, grid, removeCity, constructGraph} from "./datas";
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-
-
-
-
-const selectYellowBtn = document.getElementById('selectYellow');
-const selectWhiteBtn = document.getElementById('selectWhite');
-const selectDeleteBtn = document.getElementById('selectDelete');
+const selectYellowBtn = document.getElementById("selectYellow");
+const selectWhiteBtn = document.getElementById("selectWhite");
+const selectRockBtn = document.getElementById("selectRock");
+const selectTreeBtn = document.getElementById("selectTree");
+const selectDeleteBtn = document.getElementById("selectDelete");
+const generateBtn = document.getElementById("generate");
 
 // Flag to indicate the current selection mode
 let selectionMode = selection.BUILDING_1; // Default to yellow cube
 
 // Event listeners for UI buttons
-selectYellowBtn.addEventListener('click', function () {
+selectYellowBtn.addEventListener("click", function () {
     selectionMode = selection.BUILDING_1;
     selectedAssetsObject = FindSelectedAssetsObject();
-    console.log('Selected: Yellow Cube');
+    console.log("Selected: Yellow Cube");
 });
 
-selectWhiteBtn.addEventListener('click', function () {
+selectWhiteBtn.addEventListener("click", function () {
     selectionMode = selection.BUILDING_2;
     selectedAssetsObject = FindSelectedAssetsObject();
-    console.log('Selected: White Cube');
+    console.log("Selected: White Cube");
+});
+selectRockBtn.addEventListener("click", function () {
+    selectionMode = selection.ROCK;
+    selectedAssetsObject = FindSelectedAssetsObject();
+    console.log("Selected: Rock");
+});
+selectTreeBtn.addEventListener("click", function () {
+    selectionMode = selection.TREE;
+    selectedAssetsObject = FindSelectedAssetsObject();
+    console.log("Selected: Tree");
 });
 
-selectDeleteBtn.addEventListener('click', function () {
+selectDeleteBtn.addEventListener("click", function () {
     selectionMode = selection.DELETE;
-    console.log('Selected: Delete');
+    console.log("Selected: Delete");
+});
+generateBtn.addEventListener("click", function () {
+    generate();
 });
 
 const scene = new THREE.Scene();
-
-
 
 const camera = new THREE.PerspectiveCamera(
     45,
@@ -63,7 +73,7 @@ orbit.dampingFactor = 0.25;
 orbit.screenSpacePanning = false;
 orbit.maxPolarAngle = Math.PI / 2;
 orbit.enablePan = false;
-orbit.target.set(GRID_SIZE/2.0-0.5,0,GRID_SIZE/2.0-0.5);
+orbit.target.set(GRID_SIZE / 2.0 - 0.5, 0, GRID_SIZE / 2.0 - 0.5);
 
 camera.position.set(30, 50, 60);
 
@@ -73,17 +83,17 @@ const planeMesh = new THREE.Mesh(
     new THREE.PlaneGeometry(GRID_SIZE, GRID_SIZE), // Change size to represent a 3x3 grid
     new THREE.MeshBasicMaterial({
         side: THREE.DoubleSide,
-        visible: false
+        visible: true,
+        color: "green",
     })
 );
-planeMesh.position.set(GRID_SIZE/2.0-0.5,0,GRID_SIZE/2.0-0.5);
+planeMesh.position.set(GRID_SIZE / 2.0 - 0.5, 0, GRID_SIZE / 2.0 - 0.5,);
 planeMesh.rotateX(-Math.PI / 2);
 scene.add(planeMesh);
 
 const gridHelper = new THREE.GridHelper(GRID_SIZE, GRID_SIZE); // Change size to represent a 3x3 grid
-gridHelper.position.set(GRID_SIZE/2.0-0.5,0,GRID_SIZE/2.0-0.5);
+gridHelper.position.set(GRID_SIZE / 2.0 - 0.5, 0, GRID_SIZE / 2.0 - 0.5);
 scene.add(gridHelper);
-
 
 const light = new THREE.PointLight(0xffffff, 50);
 light.position.set(30, 20, 30);
@@ -93,48 +103,50 @@ const ambientLight = new THREE.AmbientLight();
 ambientLight.position.set(30, 20, 30);
 scene.add(ambientLight);
 
-
-
-
-
-
-
 const assets = [];
 
-
-var denemefbxObject = new Building(new THREE.Vector2(1,1),new THREE.Vector2(1,1),null,new THREE.Vector2(0,2),selection.BUILDING_1,scene,assets);
-var denemefbxObject2 = new Building(new THREE.Vector2(2,2),new THREE.Vector2(2,2),null,new THREE.Vector2(0,3),selection.BUILDING_2,scene,assets);
+var denemefbxObject = new Building(
+    new THREE.Vector2(1, 1),
+    new THREE.Vector2(1, 1),
+    null,
+    new THREE.Vector2(0, 2),
+    selection.BUILDING_1,
+    scene,
+    assets
+);
+var denemefbxObject2 = new Building(
+    new THREE.Vector2(2, 2),
+    new THREE.Vector2(2, 2),
+    null,
+    new THREE.Vector2(0, 3),
+    selection.BUILDING_2,
+    scene,
+    assets
+);
 
 var selectedAssetsObject = FindSelectedAssetsObject();
 
-
-
 const fbxLoader = new FBXLoader();
-fbxLoader.load('Assets/evyeni/ev.fbx', (object) => {
-    object.scale.set(.005, .01, .005);
-    object.rotateY(-Math.PI/2);
+fbxLoader.load("Assets/evyeni/ev.fbx", (object) => {
+    object.scale.set(0.005, 0.01, 0.005);
+    object.rotateY(-Math.PI / 2);
     //console.log(object.name);
     let light;
     let yol;
     let tas;
     object.traverse(function (child) {
-             //console.log(child);
-             if(child.type == "DirectionalLight")
-             {
-               light = child;
-             }
-             else if(child.name == "yol")
-             {
-               yol = child;
-             }
-             else if(child.name == "taslar")
-             {
-               tas = child;
-             }
-         })
-         object.remove(light,tas,yol);
-         denemefbxObject.fbxObject =object.clone();
-})
+        //console.log(child);
+        if (child.type == "DirectionalLight") {
+            light = child;
+        } else if (child.name == "yol") {
+            yol = child;
+        } else if (child.name == "taslar") {
+            tas = child;
+        }
+    });
+    object.remove(light, tas, yol);
+    denemefbxObject.fbxObject = object.clone();
+});
 
 /*fbxLoader.load('Assets/yolyeni/yeniyol.fbx', (object) => {
     object.scale.set(.005, .005, .005);
@@ -151,34 +163,31 @@ fbxLoader.load('Assets/evyeni/ev.fbx', (object) => {
     
 })*/
 
-fbxLoader.load('Assets/building.fbx', (object) => {
+fbxLoader.load("Assets/building.fbx", (object) => {
     //console.log(object);
-    object.scale.set(.025, .03, .05);
+    object.scale.set(0.025, 0.03, 0.05);
     denemefbxObject2.fbxObject = object.clone();
-})
-
-
-const mousePosition = new THREE.Vector2();
-window.addEventListener('mousemove', function (e) {
-    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
-
 });
 
+const mousePosition = new THREE.Vector2();
+window.addEventListener("mousemove", function (e) {
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+});
 
-function FindSelectedAssetsObject(){
-    if(selectedAssetsObject != null){
+function FindSelectedAssetsObject() {
+    if (selectedAssetsObject != null) {
         selectedAssetsObject.highlightMesh.visible = false;
     }
-    
+
     let selectedAssetsObjectTemp;
     for (let i = 0; i < assets.length; i++) {
-        if(selectionMode == assets[i].selectionMode){
+        if (selectionMode == assets[i].selectionMode) {
             //console.log("aaa");
             selectedAssetsObjectTemp = assets[i];
             //console.log(selectedAssetsObject.fbxObject);
             break;
-        }            
+        }
     }
     return selectedAssetsObjectTemp;
 }
@@ -188,104 +197,123 @@ export var doorPoses = [];
 const raycaster = new THREE.Raycaster();
 let intersects;
 var aa = false;
-window.addEventListener('mousedown', function (event) {
+window.addEventListener("mousedown", function (event) {
     event.stopImmediatePropagation();
     if (event.button === 0) {
         console.log("lan");
         aa = true;
         raycaster.setFromCamera(mousePosition, camera);
         intersects = raycaster.intersectObject(planeMesh);
-        
+
         if (intersects.length > 0) {
             const intersect = intersects[0];
-            var mousePosOnGrid = new THREE.Vector2(intersect.point.x,intersect.point.z).round();
-            var isIntersect = selectedAssetsObject.CheckOccupying(mousePosOnGrid,grid);
+            var mousePosOnGrid = new THREE.Vector2(
+                intersect.point.x,
+                intersect.point.z
+            ).round();
+            var isIntersect = selectedAssetsObject.CheckOccupying(
+                mousePosOnGrid,
+                grid
+            );
             //console.log(isIntersect);
-            if(!isIntersect){
+            if (!isIntersect) {
                 //console.log("inşaa");
                 const buildingClone = selectedAssetsObject.fbxObject.clone();
-                buildingClone.name = selectedAssetsObject.assetsId + " " + selectedAssetsObject.buildedId++ + " " + selectedAssetsObject.rotationDeg;
+                buildingClone.name =
+                    selectedAssetsObject.assetsId +
+                    " " +
+                    selectedAssetsObject.buildedId++ +
+                    " " +
+                    selectedAssetsObject.rotationDeg;
                 //console.log(buildingClone.name);
-                buildingClone.position.set(mousePosOnGrid.x,0,mousePosOnGrid.y);
+                buildingClone.position.set(
+                    mousePosOnGrid.x,
+                    0,
+                    mousePosOnGrid.y
+                );
                 scene.add(buildingClone);
-                selectedAssetsObject.SetOccupying(mousePosOnGrid,grid);
+                selectedAssetsObject.SetOccupying(mousePosOnGrid, grid);
                 //console.log(mousePosOnGrid.clone().add(selectedAssetsObject.doorGridPos));
-                doorPoses.push(mousePosOnGrid.clone().add(selectedAssetsObject.doorGridPos));
+                doorPoses.push(
+                    mousePosOnGrid.clone().add(selectedAssetsObject.doorGridPos)
+                );
                 console.log(buildingClone.name);
-                addCity(mousePosOnGrid.clone().add(selectedAssetsObject.doorGridPos), buildingClone.name);
-                
+                addCity(
+                    mousePosOnGrid
+                        .clone()
+                        .add(selectedAssetsObject.doorGridPos),
+                    buildingClone.name
+                );
+
                 //console.log(graph);
             }
         }
-    }
-    else if(event.button === 1)
-    {
+    } else if (event.button === 1) {
         raycaster.setFromCamera(mousePosition, camera);
-        intersects = raycaster.intersectObjects( scene.children.filter((element) => element.type =="Group"));
+        intersects = raycaster.intersectObjects(
+            scene.children.filter((element) => element.type == "Group")
+        );
         if (intersects.length > 0) {
             //console.log(intersects[0]);
             var par = intersects[0].object;
-            while(par.parent.type !== "Scene"){
+            while (par.parent.type !== "Scene") {
                 par = par.parent;
-            } 
+            }
             //console.log(doorPoses);
-            var doorPosV2 = assets[parseInt(par.name.split(" ")[0])].SetFree(new THREE.Vector2(par.position.x,par.position.z),grid,par.name.split(" ")[2], 1);
-            doorPoses = doorPoses.filter((element)=>{ return element.x !=doorPosV2.x || element.y !=doorPosV2.y;});
-            
-            
+            var doorPosV2 = assets[parseInt(par.name.split(" ")[0])].SetFree(
+                new THREE.Vector2(par.position.x, par.position.z),
+                grid,
+                par.name.split(" ")[2],
+                1
+            );
+            doorPoses = doorPoses.filter((element) => {
+                return element.x != doorPosV2.x || element.y != doorPosV2.y;
+            });
+
             scene.remove(par);
             removeCity(par.name);
             //console.log(graph);
-
         }
-    }   
-    else if(event.button === 2){
-        
-        
+    } else if (event.button === 2) {
     }
 });
-window.addEventListener('contextmenu', function (event) {
-   // event.preventDefault();
+window.addEventListener("contextmenu", function (event) {
+    // event.preventDefault();
 });
 
-window.addEventListener('keydown', function (event) {    
+window.addEventListener("keydown", function (event) {
     event.stopImmediatePropagation();
     console.log("tuşa basıldı");
     switch (event.key) {
-        case 'w':
+        case "w":
             // Move camera forward
             camera.position.z -= 1;
             break;
-        case 's':
+        case "s":
             // Move camera backward
             camera.position.z += 1;
             break;
-        case 'a':
+        case "a":
             // Move camera left
             camera.position.x -= 1;
             break;
-        case 'd':
+        case "d":
             // Move camera right
             camera.position.x += 1;
             break;
-        case 'e':
-            selectedAssetsObject.RotateCCW(-Math.PI/2);
+        case "e":
+            selectedAssetsObject.RotateCCW(-Math.PI / 2);
             break;
-        case 'q':
-            selectedAssetsObject.RotateCCW(Math.PI/2);
+        case "q":
+            selectedAssetsObject.RotateCCW(Math.PI / 2);
             break;
-        case 'z':
-            var mst = constructGraph();
-            if(mst != null){
-                console.log(mst);
-            }
-            break;
+        case "z":
     }
 });
 
 var start = 0;
-function animate(time) {
 
+function animate(time) {
     //console.log(1000/(time-start));
     start = time;
     raycaster.setFromCamera(mousePosition, camera);
@@ -294,30 +322,49 @@ function animate(time) {
     if (intersects.length > 0) {
         selectedAssetsObject.highlightMesh.visible = true;
         const intersect = intersects[0];
-        var mousePosOnGrid = new THREE.Vector2(intersect.point.x,intersect.point.z).round();
-        selectedAssetsObject.highlightMesh.position.set(mousePosOnGrid.x,0,mousePosOnGrid.y);        
+        var mousePosOnGrid = new THREE.Vector2(
+            intersect.point.x,
+            intersect.point.z
+        ).round();
+        selectedAssetsObject.highlightMesh.position.set(
+            mousePosOnGrid.x,
+            0,
+            mousePosOnGrid.y
+        );
 
-        if (selectedAssetsObject.CheckOccupying(mousePosOnGrid,grid)) {
-           selectedAssetsObject.highlightMeshMaterial.color.set(0xFF0000);
+        if (selectedAssetsObject.CheckOccupying(mousePosOnGrid, grid)) {
+            selectedAssetsObject.highlightMeshMaterial.color.set(0xff0000);
         } else {
-            selectedAssetsObject.highlightMeshMaterial.color.set(0x00FF00);
+            selectedAssetsObject.highlightMeshMaterial.color.set(0x00ff00);
         }
-
-    }
-    else{
+    } else {
         selectedAssetsObject.highlightMesh.visible = false;
     }
 
     orbit.update();
-    
-    selectedAssetsObject.highlightMesh.material.opacity = 1 + Math.sin(time / 120);
+
+    selectedAssetsObject.highlightMesh.material.opacity =
+        1 + Math.sin(time / 120);
     renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function () {
+window.addEventListener("resize", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+const generate = () => {
+    resetRoads();
+
+    var mst = constructGraph();
+    if (mst != null) {
+        console.log(mst);
+    }
+
+    for (let i = 0; i < mst; i++) {
+        //addRoad(kordinat, obje)
+    }
+};
--- objects.js
+++ objects.js
@@ -1,87 +1,90 @@
-import { Vector2,Vector3,Mesh,PlaneGeometry,MeshBasicMaterial,DoubleSide} from "three";
-import { degToRad, radToDeg } from "three/src/math/MathUtils";
+import {Vector2, Vector3, Mesh, PlaneGeometry, MeshBasicMaterial, DoubleSide} from "three";
+import {degToRad, radToDeg} from "three/src/math/MathUtils";
+import {roads} from "./datas";
 //import * as THREE from 'three';
 
 var assetsId = 0;
 
 
 export class AssetsObject {
-    constructor(topLeftGridPos,bottomRightGridPos,fbxObject,selectionMode,scene,assets) {
+    constructor(topLeftGridPos, bottomRightGridPos, fbxObject, selectionMode, scene, assets) {
         this.selectionMode = selectionMode;
         this.fbxObject = fbxObject;
         this.assetsId = assetsId++;
         this.buildedId = 0;
         this.rotationDeg = 0;
         this.occupiedGridPoses = [];
-        this.highlightMeshMaterial = new MeshBasicMaterial({ color: 0xff0000 ,side: DoubleSide,
-            transparent: true});
-        this.highlightMesh =generateMesh2(topLeftGridPos.x + bottomRightGridPos.x + 1
-            , topLeftGridPos.y + bottomRightGridPos.y + 1,this.highlightMeshMaterial); 
+        this.highlightMeshMaterial = new MeshBasicMaterial({
+            color: 0xff0000, side: DoubleSide,
+            transparent: true
+        });
+        this.highlightMesh = generateMesh2(topLeftGridPos.x + bottomRightGridPos.x + 1
+            , topLeftGridPos.y + bottomRightGridPos.y + 1, this.highlightMeshMaterial);
         this.highlightMesh.visible = false;
-        
+
         this.highlightMesh.rotateX(-Math.PI / 2);
-        this.highlightMesh.geometry.translate( (bottomRightGridPos.x-topLeftGridPos.x)/2.0  
-                                            ,(topLeftGridPos.y-bottomRightGridPos.y)/2.0 ,0);
+        this.highlightMesh.geometry.translate((bottomRightGridPos.x - topLeftGridPos.x) / 2.0
+            , (topLeftGridPos.y - bottomRightGridPos.y) / 2.0, 0);
 
         for (let x = -topLeftGridPos.x; x <= bottomRightGridPos.x; x++) {
             for (let z = -topLeftGridPos.y; z <= bottomRightGridPos.y; z++) {
-                if(x== 0 && z==0){
+                if (x == 0 && z == 0) {
                     continue;
                 }
-                this.occupiedGridPoses.push(new Vector2(x,z));
-            }            
+                this.occupiedGridPoses.push(new Vector2(x, z));
+            }
         }
-        this.occupiedGridPoses.push(new Vector2(0,0));
+        this.occupiedGridPoses.push(new Vector2(0, 0));
         scene.add(this.highlightMesh);
         assets.push(this);
     }
 
     RotateCCW(radian) {
-        this.rotationDeg = (this.rotationDeg+radToDeg(radian)+360)%360;
-        
+        this.rotationDeg = (this.rotationDeg + radToDeg(radian) + 360) % 360;
+
         this.occupiedGridPoses.forEach(element => {
-            element.rotateAround(new Vector2(0,0),-radian).round();
+            element.rotateAround(new Vector2(0, 0), -radian).round();
         });
         this.highlightMesh.rotateZ(radian);
         //FBX OBJESİNİ DE ROTATE ET
-        this.fbxObject.rotateOnWorldAxis(new Vector3(0,1,0),radian);
-        
+        this.fbxObject.rotateOnWorldAxis(new Vector3(0, 1, 0), radian);
+
     }
 
-    CheckOccupying(mouseVec2,grid){
+    CheckOccupying(mouseVec2, grid) {
         var isIntersect = false;
         for (let i = 0; i < this.occupiedGridPoses.length; i++) {
             var temp = mouseVec2.clone().add(this.occupiedGridPoses[i]);
             //console.log(temp);
-            if(temp.y >= grid.length || temp.x >= grid[0].length || temp.y<0 || temp.x<0 || grid[temp.y][temp.x]){
+            if (temp.y >= grid.length || temp.x >= grid[0].length || temp.y < 0 || temp.x < 0 || grid[temp.y][temp.x]) {
                 isIntersect = true;
-                break; 
+                break;
             }
         }
         return isIntersect;
     }
 
-    SetOccupying(mouseVec2,grid){
+    SetOccupying(mouseVec2, grid) {
         for (let i = 0; i < this.occupiedGridPoses.length; i++) {
             var temp = mouseVec2.clone().add(this.occupiedGridPoses[i]);
             grid[temp.y][temp.x] = true;
         }
     }
 
-    SetFree(centerGridPos,grid,rotationDegTemp){
+    SetFree(centerGridPos, grid, rotationDegTemp) {
         let doorPos;
-        var newRad = degToRad(parseInt(rotationDegTemp)-this.rotationDeg);
+        var newRad = degToRad(parseInt(rotationDegTemp) - this.rotationDeg);
         this.occupiedGridPoses.forEach(element => {
-            element.rotateAround(new Vector2(0,0),-newRad).round();
+            element.rotateAround(new Vector2(0, 0), -newRad).round();
         });
         for (let i = 0; i < this.occupiedGridPoses.length; i++) {
             var temp = centerGridPos.clone().add(this.occupiedGridPoses[i]);
-            
+
             grid[temp.y][temp.x] = false;
         }
         doorPos = centerGridPos.clone().add(this.doorGridPos);
         this.occupiedGridPoses.forEach(element => {
-            element.rotateAround(new Vector2(0,0),newRad).round();
+            element.rotateAround(new Vector2(0, 0), newRad).round();
         });
         console.log(doorPos);
         return doorPos;
@@ -89,28 +92,27 @@ export class AssetsObject {
 }
 
 export class Building extends AssetsObject {
-    constructor(topLeftGridPos,bottomRightGridPos,fbxObject, doorGridPos,selectionMode,scene,assets) {
-        super(topLeftGridPos,bottomRightGridPos,fbxObject,selectionMode,scene,assets);
+    constructor(topLeftGridPos, bottomRightGridPos, fbxObject, doorGridPos, selectionMode, scene, assets) {
+        super(topLeftGridPos, bottomRightGridPos, fbxObject, selectionMode, scene, assets);
         this.doorGridPos = doorGridPos;
         this.occupiedGridPoses.push(doorGridPos);
-        var doorMesh = generateMesh(1,this.highlightMeshMaterial);
-        doorMesh.position.set(doorGridPos.x,-doorGridPos.y,0);
+        var doorMesh = generateMesh(1, this.highlightMeshMaterial);
+        doorMesh.position.set(doorGridPos.x, -doorGridPos.y, 0);
         this.highlightMesh.add(doorMesh);
         this.highlightMesh.visible = false;
-    }   
+    }
 }
 
 
-
-function generateMesh(size,material) {
+function generateMesh(size, material) {
     return new Mesh(
-        new PlaneGeometry(size, size), 
+        new PlaneGeometry(size, size),
         material);
 }
 
- function generateMesh2(size1,size2,material) {
+function generateMesh2(size1, size2, material) {
     return new Mesh(
-        new PlaneGeometry(size1, size2), 
+        new PlaneGeometry(size1, size2),
         material);
 }
 
@@ -122,11 +124,10 @@ class Road {
 
 }
 
-export function constructRoad(coords, object) {
-    roads.push(new Road(coords, object));
+export function resetRoads() {
+    roads.length = 0;
+}
 
-    const buildingClone = object.fbxObject.clone();
-    buildingClone.position.set(coords.x, 0, coords.y);
-    scene.add(buildingClone);
-    //object.SetOccupying(coords, grid);
+export function addRoad(coords, object) {
+    roads.push(new Road(coords, object));
 }
